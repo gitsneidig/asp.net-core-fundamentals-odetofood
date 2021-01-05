9b31fa8882a103a7a0c2fca3ca5e8918488ca9f1
gitsneidig <scott@scottneidig.com>
2021-01-05 16:08:59 -0600

    Added validation checks using DataAnnotations in Restaurant class def

--- OdeToFood/OdeToFood.Core/Restaurant.cs
+++ OdeToFood/OdeToFood.Core/Restaurant.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace OdeToFood.Core
@@ -7,8 +8,11 @@ namespace OdeToFood.Core
 
     public class Restaurant
     {
+
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Location { get; set; }
         public CuisineType Cuisine { get; set; }
     }
--- OdeToFood/OdeToFood/Pages/Restaurants/Edit.cshtml.cs
+++ OdeToFood/OdeToFood/Pages/Restaurants/Edit.cshtml.cs
@@ -39,6 +39,7 @@ namespace OdeToFood.Pages.Restaurants
 
         public IActionResult OnPost()
         {
+            Cuisines = htmlHelper.GetEnumSelectList<CuisineType>();
             restaurantData.Update(Restaurant);
             restaurantData.Commit();
             return Page();
