4fec645a31c72b2f4b12a844d132b82c1276ac8d
gitsneidig <scott@scottneidig.com>
2021-01-05 16:54:07 -0600

    Handled Add new page in the Edit page code behind

--- OdeToFood/OdeToFood/Pages/Restaurants/Edit.cshtml.cs
+++ OdeToFood/OdeToFood/Pages/Restaurants/Edit.cshtml.cs
@@ -47,16 +47,22 @@ namespace OdeToFood.Pages.Restaurants
 
         public IActionResult OnPost()
         {
-            if(ModelState.IsValid)
+            if(!ModelState.IsValid)
             {
-                restaurantData.Update(Restaurant);
-                restaurantData.Commit();
-                return RedirectToPage("./Detail", new { restaurantId = Restaurant.Id });
+                Cuisines = htmlHelper.GetEnumSelectList<CuisineType>();
+                return Page();
             }
 
-            Cuisines = htmlHelper.GetEnumSelectList<CuisineType>();
-            
-            return Page();
+            if(Restaurant.Id > 0) { 
+                restaurantData.Update(Restaurant);
+                restaurantData.Commit();
+            }
+            else
+            {
+                restaurantData.Add(Restaurant);
+            }
+            restaurantData.Commit();
+            return RedirectToPage("./Detail", new { restaurantId = Restaurant.Id });
         }
     }
 }
