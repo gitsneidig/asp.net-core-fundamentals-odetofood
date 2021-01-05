a88c4162a95aed9e1966fb71d684d19156860182
gitsneidig <scott@scottneidig.com>
2021-01-05 16:49:07 -0600

    Add and Add() method to the Data access service

--- OdeToFood/OdeToFood.Data/IRestaurantData.cs
+++ OdeToFood/OdeToFood.Data/IRestaurantData.cs
@@ -11,6 +11,7 @@ namespace OdeToFood.Data
         IEnumerable<Restaurant> GetRestaurantsByName(string name);
         Restaurant GetById(int Id);
         Restaurant Update(Restaurant updatedRestaurant);
+        Restaurant Add(Restaurant newRestaurant);
         int Commit();
     }
 
@@ -31,6 +32,13 @@ namespace OdeToFood.Data
         public int Commit()
         {
             return 0;
+        }
+
+        public Restaurant Add(Restaurant newRestaurant) 
+        {
+            restaurants.Add(newRestaurant);
+            newRestaurant.Id = restaurants.Max(r => r.Id) + 1;
+            return newRestaurant;
         }
 
         public Restaurant Update(Restaurant updatedRestaurant)
