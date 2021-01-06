57f9c62bbfd1b44997cc2923c7a3ba52fe9dd226
gitsneidig <scott@scottneidig.com>
2021-01-06 17:53:15 -0600

    Wire connection string up to DbContext class

--- OdeToFood/OdeToFood/Startup.cs
+++ OdeToFood/OdeToFood/Startup.cs
@@ -11,6 +11,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using OdeToFood.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace OdeToFood
 {
@@ -26,6 +27,12 @@ namespace OdeToFood
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddDbContextPool<OdeToFoodDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("OdeToFoodDb"));
+            });
+
             services.AddSingleton<IRestaurantData, InMemoryRestaurantData>();
 
             services.Configure<CookiePolicyOptions>(options =>
